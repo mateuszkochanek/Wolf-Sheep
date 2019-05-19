88c10cccc11d04f2147b2c115609a4844cfc1c7b
Erthax <mateuszkochanek01@gmail.com>
2019-05-19 14:24:56 +0200

    #00.Repo and Project Creation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Wolf-Sheep
+Symulacja stworzona na potrzeby laboratoriów z Kursu Programowania w 2019.
---
+++ Wolf&Sheep/src/main/java/Wolf&Sheep/App.java
@@ -0,0 +1,13 @@
+package Wolf&Sheep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
